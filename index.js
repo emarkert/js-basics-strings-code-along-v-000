45c15e0e51a259927ff01419fa4d4dbedaaf4697
Evan  Markert <evanmarkert@gmail.com>
2019-08-02 03:00:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
-let currentUser = 'Grace Hopper';
-let welcomeMessage = 'Welcome to Flatbook, ' + ${currentUser}!`;
+const currentUser = 'Evan Markert';
+const welcomeMessage = 'Welcome to Flatbook, ' + ${currentUser}!`;
